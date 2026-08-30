af2ddd72f75da21dd318b410514f3eb9d14e4618
LucarioGM01 <121914071+LucarioGM01@users.noreply.github.com>
2026-08-30 15:16:02 -0600

    feat: completar tareas

--- app/tasks.py
+++ app/tasks.py
@@ -11,3 +11,25 @@ def agregar_tarea(tareas, titulo):
     tareas.append(tarea)
 
     return True
+def agregar_tarea(tareas, titulo):
+    if titulo.strip() == "":
+        return False
+
+    tarea = {
+        "id": len(tareas) + 1,
+        "titulo": titulo.strip(),
+        "completada": False
+    }
+
+    tareas.append(tarea)
+
+    return True
+
+
+def completar_tarea(tareas, id_tarea):
+    for tarea in tareas:
+        if tarea["id"] == id_tarea:
+            tarea["completada"] = True
+            return True
+
+    return False
