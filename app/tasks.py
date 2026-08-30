23b4e41a5c42dd47e5332c66e038aa988e2596cb
LucarioGM01 <121914071+LucarioGM01@users.noreply.github.com>
2026-08-30 15:19:01 -0600

    feat: eliminar tareas

--- app/tasks.py
+++ app/tasks.py
@@ -33,3 +33,34 @@ def completar_tarea(tareas, id_tarea):
             return True
 
     return False
+def agregar_tarea(tareas, titulo):
+    if titulo.strip() == "":
+        return False
+
+    tarea = {
+        "id": len(tareas) + 1,
+        "titulo": titulo.strip(),
+        "completada": False
+    }
+
+    tareas.append(tarea)
+
+    return True
+
+
+def completar_tarea(tareas, id_tarea):
+    for tarea in tareas:
+        if tarea["id"] == id_tarea:
+            tarea["completada"] = True
+            return True
+
+    return False
+
+
+def eliminar_tarea(tareas, id_tarea):
+    for tarea in tareas:
+        if tarea["id"] == id_tarea:
+            tareas.remove(tarea)
+            return True
+
+    return False
