3eb9fa295d6ded89d8b17741e4c65dbf95c0e262
Leo-0611 <leonardobelcast06@gmail.com>
2026-08-30 16:34:03 -0600

    feat: cargar tareas desde json

--- app/storage.py
+++ app/storage.py
@@ -4,3 +4,10 @@ import json
 def guardar_tareas(tareas):
     with open("data/tasks.json", "w", encoding="utf-8") as archivo:
         json.dump(tareas, archivo, ensure_ascii=False, indent=4)
+        
+def cargar_tareas():
+    try:
+        with open("data/tasks.json", "r", encoding="utf-8") as archivo:
+            return json.load(archivo)
+    except FileNotFoundError:
+        return []
